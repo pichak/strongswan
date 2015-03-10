a3c67eea44baae874f0555b377a589bdc15113a9
Amir Sabbaghi <asaba90@gmail.com>
2015-03-10 12:25:45 +0330

    fix typo in renaming install.sh

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 set -ex
 
 docker rm strongswan-build || true
-docker run --name strongswan-build -v $PWD:/root/ ubuntu bash /root/install.sh
+docker run --name strongswan-build -v $PWD:/root/ ubuntu bash /root/build.sh
 cd build/
 tar -c * | xz - > ../strongswan.tar.xz
 cd ..
